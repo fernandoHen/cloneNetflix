af0ad2ac401f05d63083824356c54845f96f7b11
fernando.cossa <fernando.cossa@projuris.com.br>
2022-10-09 20:25:54 -0300

    PROJ-0002

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+<<<<<<< Updated upstream
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent
+=======
+import { FullBannerComponent } from './shared/full-banner/full-banner.component';
+import { ListTitlesComponent } from './shared/list-titles/list-titles.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    FullBannerComponent,
+    ListTitlesComponent,
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
+<<<<<<< Updated upstream
   bootstrap: [AppComponent, NavBarComponent]
+=======
+  bootstrap: [AppComponent, NavBarComponent, FullBannerComponent, ListTitlesComponent]
+>>>>>>> Stashed changes
 })
 export class AppModule { }
